83948648ac28b97b4c693365faebef378d7ca85a
Marvin <mmelitan@asu.edu>
2017-10-17 10:17:19 -0700

    First commit

---
+++ contact-us.php
@@ -0,0 +1,22 @@
+<?php 
+	$title = "Contact Me";
+	$subtitle = 'Questions? I might have answers';
+	$image_url = 'squidmin banner_v2.png';
+	$author = '';
+	$author_link = '';
+	$date = '';
+?>
+<?php require_once("header.php"); ?>
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <p>Want to get in touch with me? Contact me directly at <a href="mailto:squidmin00@gmail.com">squidmin00@gmail.com</a> and I will try to get back to you!</p>
+                
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+<?php require_once("footer.php"); ?>
---
+++ footer.php
@@ -0,0 +1,55 @@
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Squidmin.com 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+	<!--<script
+			  src="https://code.jquery.com/jquery-3.0.0.js"
+			  integrity="sha256-jrPLZ+8vDxt2FnE1zvZXCkCcebI/C8Dt5xyaQBjxQIo="
+			  crossorigin="anonymous"></script>
+	-->
+	<script src="<?php home_url(); ?>js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php home_url(); ?>js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
